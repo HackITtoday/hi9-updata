c3c2aa1cc833999aafa1fb4bc68458c04bfc8c80
mywoisme <mywoisme@gmail.com>
2015-06-29 13:26:17 -0400

    started card changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,14 +39,15 @@
 
 	  _gs('GSN-963825-Q');
 		
+	
 //	</script>	
-//  <!-- end of gosquared coce  -->
+//  <!-- end of gosquared code  -->
 
-//	<!-- Start of hi9help Zendesk Widget script -->
-//		<script>
-                /*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","hi9help.zendesk.com");/*]]>*/
-                //</script>
-//	<!-- End of hi9help Zendesk Widget script -->
+// <!-- Start of hi9help Zendesk Widget script -->
+//  <script>
+		/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","hi9help.zendesk.com");/*]]>*/
+//	</script>
+// <!-- End of hi9help Zendesk Widget script -->
 
 //	<!-- Google Analytics -->
 //	<script>
@@ -59,4 +60,16 @@
 		  ga('send', 'pageview');
 //	</script>
 //	<!-- End of Google Analytics -->
+	
+//	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NH24M5"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	// <script>
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-NH24M5');
+	// </script>
+//	<!-- End Google Tag Manager -->
 })(document);
